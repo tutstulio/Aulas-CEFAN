28230b67a7115517057265e448b54258ca95918b
Tulio Braz <tuliogbraz@hotmail.com>
2025-06-10 13:34:14 -0300

    Melhorias no Design
    Inclusão de uma Logo, design da home refeito e outras estilizações, como nos links e nas fontes das demais páginas.

--- Site-CEFAN/Esportes/boliche.js
+++ Site-CEFAN/Esportes/boliche.js
@@ -7,6 +7,14 @@ const peso = document.getElementById('peso');
 const xp = document.getElementById('xp');
 const mao = document.getElementById('mao');
 
+let data_aaaa_mm_dd = [];
+let pontos = [];
+let strikes = [];
+let spares = [];
+let splits = [];
+let opens = [];
+let obs = [];
+
 nome.textContent = thisUser.username;
 idade.textContent = `Idade: ${thisUser.idade} anos`;
 altura.textContent = `Altura: ${thisUser.altura} m`;
@@ -20,35 +28,60 @@ else
 
 function adicionar ()
 {
-    const data_aaaa_mm_dd = document.getElementById('data').value;
-    const pontos = document.getElementById('pontos').value;
-    const strikes = document.getElementById('strikes').value;
-    const spares = document.getElementById('spares').value;
-    const splits = document.getElementById('splits').value;
-    const opens = document.getElementById('opens').value;
-    const obs = document.getElementById('obs').value;
+    data_aaaa_mm_dd.push(document.getElementById('data').value);
+    pontos.push(document.getElementById('pontos').value);
+    strikes.push(document.getElementById('strikes').value);
+    spares.push(document.getElementById('spares').value);
+    splits.push(document.getElementById('splits').value);
+    opens.push(document.getElementById('opens').value);
+    obs.push(document.getElementById('obs').value);
 
+    const len = obs.length;
     const recordsTable = document.querySelector('.records tbody');
     const metricsTableRow = document.querySelector('.metrics tbody tr');
     const novaLinha = document.createElement('tr');
 
     novaLinha.innerHTML = `
-        <td>${data_aaaa_mm_dd}</td>
-        <td>${pontos}</td>
-        <td>${strikes}</td>
-        <td>${spares}</td>
-        <td>${splits}</td>
-        <td>${opens}</td>
-        <td>${obs}</td>
+        <td>${data_aaaa_mm_dd[len-1]}</td>
+        <td>${pontos[len-1]}</td>
+        <td>${strikes[len-1]}</td>
+        <td>${spares[len-1]}</td>
+        <td>${splits[len-1]}</td>
+        <td>${opens[len-1]}</td>
+        <td>${obs[len-1]}</td>
     `;
 
     recordsTable.appendChild(novaLinha);
 
     const total = recordsTable.childElementCount - 1;
 
-    metricsTableRow.children[0].textContent = "A dias/mês";
-    metricsTableRow.children[1].textContent = 45;
-    metricsTableRow.children[2].textContent = 45.2;
-    metricsTableRow.children[3].textContent = 5.7;
+    metricsTableRow.children[0].textContent = data_aaaa_mm_dd[melhor(pontos)];
+    metricsTableRow.children[1].textContent = media(pontos);
+    metricsTableRow.children[2].textContent = 0;
+    metricsTableRow.children[3].textContent = 0;
     metricsTableRow.children[4].textContent = total + " dias";
+}
+
+function melhor(valores) {
+    const N = valores.length;
+    let maior = 0;
+    let melhor;
+    for (let i = 0; i < N; i++) {
+        const v = Number(valores[i]);
+        if (v > maior) {
+            maior = v;
+            melhor = i;
+        }
+    }
+    return melhor;
+}
+
+function media(valores) {
+    const N = valores.length;
+    let media = 0;
+    for (let i = 0; i < N; i++) {
+        media += Number(valores[i]);
+    }
+    media /= N;
+    return media;
 }
